013ce7b22a6b53fe3c24ab30cddacd704452354c
leoaltemari <leo.altemari@gmail.com>
2023-04-11 23:01:00 -0300

    Configuring index file and express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello world');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import mongoose, { ConnectOptions } from 'mongoose';
+
+dotenv.config();
+
+const app: Express 	= express();
+const port 					= process.env.PORT;
+const router 				= express.Router();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+// Load Routes
+// const indexRoute = require("./routes/index-route");
+// const userRoute = require("./routes/user-route");
+// const eventRoute = require("./routes/event-route");
+
+// Routes
+// app.use("/", indexRoute);
+// app.use("/user", userRoute);
+// app.use("/events", eventRoute);
+
+// Connect to the DB
+const uri = process.env.DB_CONN_STR as string;
+
+mongoose.connect(uri)
+	.then(() => console.log("MongoDB Connected…"))
+	.catch(err => console.log(err));
+
+
+app.listen(port, () => console.log(`⚡️[server]: Server is running at http://localhost:${port}`));
